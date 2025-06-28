70830355fb39710e78db911979fc8c1aac80a6aa
Ismail Mohammed <ismail.bitte@gmail.com>
2025-06-28 00:46:43 -0500

    added @component to repository class

--- src/main/java/application/repositories/ParkingLotRepository.java
+++ src/main/java/application/repositories/ParkingLotRepository.java
@@ -1,12 +1,13 @@
 package application.repositories;
 
 import application.models.ParkingLot;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Repository
 public class ParkingLotRepository {
     private List<ParkingLot> parkingLots = new ArrayList<>();
 
